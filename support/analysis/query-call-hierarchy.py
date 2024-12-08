934df52f64349474cf8ec19fe56d96290eb08451
Travis B. Hartwell <nafai@travishartwell.net>
2024-12-08 13:20:23 -0800

    Show unique callers in query-call-hierarchy

--- support/analysis/query-call-hierarchy.py
+++ support/analysis/query-call-hierarchy.py
@@ -6,19 +6,23 @@ from pathlib import Path
 import sys
 
 
-def query_stack_frames(stack_frames: list[trace_logs.StackFrame], target_function: str):
+def query_stack_frames(
+    stack_frames: list[trace_logs.StackFrame], target_function: str
+) -> tuple[set[tuple], set[str]]:
     related_stack_frames = [
         f for f in stack_frames if f.call_stack[-1] == target_function
     ]
     unique_call_stacks = set()
+    unique_callers = set()
 
     for frame in related_stack_frames:
         call_stack = tuple(
             fn for fn in frame.call_stack if fn not in ["main", "source"]
         )
+        unique_callers.add(call_stack[-2])
         unique_call_stacks.add(call_stack)
 
-    return unique_call_stacks
+    return unique_call_stacks, unique_callers
 
 
 def main():
@@ -39,16 +43,24 @@ def main():
 
     stack_frames = trace_logs.load_stack_frames_from_trace_log(trace_log_file)
 
-    unique_call_stacks = query_stack_frames(stack_frames, target_function)
+    unique_call_stacks, unique_callers = query_stack_frames(
+        stack_frames, target_function
+    )
 
     print(f"The following unique call stacks were found calling '{target_function}':")
-    print("---------\n")
+    print("---------")
 
     for stack in unique_call_stacks:
         stack = list(stack)
 
         print(" -> ".join(stack))
-        print("---------\n")
+        print("---------")
+
+    print(f"The following unique callers called '{target_function}':")
+    print("---------")
+
+    for caller in sorted(unique_callers):
+        print(f"{caller}")
 
 
 if __name__ == "__main__":
